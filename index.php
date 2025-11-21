634ab8713726911f2a03e390d5fd3eb57ff586a5
kuuhaku1102 <j-@infintydesign.online>
2025-11-21 22:41:10 +0900

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,22 @@ if ( ! defined( 'ABSPATH' ) ) {
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="ライブチャットの遊び方、料金体系、安全性、主要サイト比較まで徹底解説。初心者にもわかりやすく、人気のライブチャット女性プロフィール一覧も掲載中。">
+    <!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.sakura.ne.jp/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '7']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
