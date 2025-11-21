2663041c3d34e564cbf7828524d8f16032756b84
kuuhaku1102 <j-@infintydesign.online>
2025-11-21 20:58:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+    <p>テスト</p>
 <div class="lcd-container">
     <header class="lcd-header">
         <h1><?php bloginfo( 'name' ); ?></h1>
